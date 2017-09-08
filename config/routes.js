03e612db669f09e71c0a3dfb24e2edcafd469b28
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-09-08 12:08:14 -0700

    delete instruction and ingredient functionality

--- config/routes.js
+++ config/routes.js
@@ -42,13 +42,16 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  '/create'   :'RecipeController.create',
-  '/'         :'RecipeController.read',
-  '/update'   :'RecipeController.update',
-  '/delete'   :'RecipeController.delete',
-  '/search':'RecipeController.search',
-  '/createinstruction':'RecipeController.createInstruction',
-  '/:id/createinstruction':'RecipeController.createInstruction',
-  '/:id'      :'RecipeController.show'
+  '/create'                           :'RecipeController.create',
+  '/'                                 :'RecipeController.read',
+  '/update'                           :'RecipeController.update',
+  '/deleteinstruction'                :'RecipeController.deleteinstruction',
+  '/:id/deleteinstruction/:ins_id'    :'RecipeController.deleteinstruction',
+  '/deleteingredient'                 :'RecipeController.deleteingredient',
+  '/:id/deleteingredient/:ing_id'     :'RecipeController.deleteingredient',
+  '/search'                           :'RecipeController.search',
+  '/createinstruction'                :'RecipeController.createInstruction',
+  '/:id/createinstruction'            :'RecipeController.createInstruction',
+  '/:id'                              :'RecipeController.show'
   // '/createInstruction':'RecipeController.read'
 };
